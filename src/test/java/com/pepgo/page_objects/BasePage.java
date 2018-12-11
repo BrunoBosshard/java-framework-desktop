8a760b391c269b502469e3ee539a486c23436a68
Bruno Bosshard <bruno.bosshard@pepgo.com>
2018-12-11 16:14:40 +1000

    Initial commit

---
+++ src/test/java/com/pepgo/page_objects/BasePage.java
@@ -0,0 +1,18 @@
+package com.pepgo.page_objects;
+
+import com.pepgo.DriverBase;
+import java.net.MalformedURLException;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public abstract class BasePage {
+
+    protected RemoteWebDriver driver;
+
+    public BasePage() {
+        try {
+            driver = DriverBase.getDriver();
+        } catch (MalformedURLException ignored) {
+            //This will be be thrown when the test starts if it cannot connect to a RemoteWebDriver Instance
+        }
+    }
+}
