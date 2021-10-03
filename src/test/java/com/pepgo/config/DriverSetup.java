4df31c110ce7fa21ceecd1dbd57a454ccce93db3
Bruno Bosshard <bruno.bosshard@pepgo.com>
2021-10-03 16:36:07 +1100

    Delete src/test directory

--- src/test/java/com/pepgo/config/DriverSetup.java
+++
@@ -1,8 +0,0 @@
-package com.pepgo.config;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-public interface DriverSetup {
-    RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities);
-}
